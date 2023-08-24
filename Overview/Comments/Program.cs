3a1055322dc1c59fc6cce9bef1d71aa6169ddfe8
Peter Cordero <petercordero29@gmail.com>
2023-08-24 18:00:49 -0400

    done

---
+++ Overview/Comments/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Comments
+{
+    class Program
+    {
+        /// XML Comments are used to help provide documentation
+        /// They start with triple-slashes and have a special syntax 
+        /// <summary>
+        /// XML Comments are used to help provide documentation
+        /// </summary>
+        /// <param name="args"></param> 
+        /// <returns>
+        /// no return value
+        /// </returns>
+        static void Main(string[] args)
+        {
+            // Single line comments
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
